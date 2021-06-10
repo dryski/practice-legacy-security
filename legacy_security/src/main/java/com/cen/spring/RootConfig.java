98fb739e63a77884e5f44836836010922e345baf
Kim sejun <semineun@naver.com>
2021-06-10 17:52:42 +0900

    Create RootConfig.java

---
+++ legacy_security/src/main/java/com/cen/spring/RootConfig.java
@@ -0,0 +1,10 @@
+package com.cen.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.cen"})
+public class RootConfig {
+	
+}
