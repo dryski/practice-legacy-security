f211fc2120e8b49e0f16c928024abf3f77d07119
Kim sejun <semineun@naver.com>
2021-06-10 17:52:04 +0900

    Create AppInitializer.java

---
+++ legacy_security/src/main/java/com/cen/AppInitializer.java
@@ -0,0 +1,29 @@
+package com.cen;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitializer implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(final ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
+		root.scan("com.cen");
+		servletContext.addListener(new ContextLoaderListener(root));
+		
+		ServletRegistration.Dynamic appServlet = servletContext.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
+		appServlet.setLoadOnStartup(1);
+		appServlet.addMapping("/");
+		
+		servletContext.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain"))
+		.addMappingForUrlPatterns(null, false, "/*");
+	}
+}
